4a1f63e2ca10ff3c230d253adafeaf30326f9d26
huangyijun <huangyijun@huangyijun.local>
2017-03-10 11:10:23 +0800

    add load config methods

---
+++ config/config.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = {
+    MESSAGE_TYPE: {
+        0: 'HEART_BEAT_REQUEST',
+        1: 'HEART_BEAT_RESPONSE',
+        2: 'DO_CPU_PROFILER',
+        3: 'CPU_PROFILER_MESSAGE'
+    },
+    TCP_CONNECTION_CONFIG: {
+        host: '127.0.0.1',
+        port: 8082
+    }
+};
