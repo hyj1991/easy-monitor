efc042247f796b9fc76ca00d240d5038466b36c3
huangyijun <huangyijun@huangyijun.local>
2017-03-20 15:22:03 +0800

    fix a re-profiling bug

--- main_process/src/cpuprofiler.js
+++ main_process/src/cpuprofiler.js
@@ -1,18 +1,24 @@
 'use strict';
 const v8Profiler = require('v8-profiler');
+const EventEmitter = require('events').EventEmitter;
+const event = new EventEmitter();
 
 let isDoingProfiling = false;
 
 function doCpuProfiler(title, config, callback) {
     if (isDoingProfiling) {
+        event.once('cpu_profiling_complete', callback);
         return;
     }
+    isDoingProfiling = true;
     v8Profiler.startProfiling(title, true);
 
     setTimeout(() => {
         let profiler = v8Profiler.stopProfiling(title);
         profiler.delete();
         callback(profiler);
+        event.emit('cpu_profiling_complete', profiler);
+        isDoingProfiling = false;
     }, config.CPU_PROFILING_TIME)
 }
 
