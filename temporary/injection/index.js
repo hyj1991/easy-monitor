1746aa9b1c44ec7b12b22d98e994a9a5f111dad8
huangyijun <huangyijun@huangyijun.local>
2017-07-01 23:15:22 +0800

    update document & package.json & tag to release v2.1.2

--- temporary/injection/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-const cpuProfiler = require('./lib/cpu');
-
-module.exports = function easyMonitor(dashbord) {
-    //Start a Tcp Client, that connect to Fork Process
-    cpuProfiler.call(null, require('./lib/helper').loadConfig(), dashbord);
-};
