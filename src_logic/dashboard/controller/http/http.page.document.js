808be4b2092893e75fc49aa83a34a682b098d9ff
huangyijun <huangyijun@huangyijun.local>
2017-07-14 22:24:46 +0800

    static resource bug fixed (#20)

--- src_logic/dashboard/controller/http/http.page.document.js
+++ src_logic/dashboard/controller/http/http.page.document.js
@@ -1,4 +1,5 @@
 'use strict';
+const fs = require('fs');
 const path = require('path');
 
 module.exports = function (app) {
@@ -12,13 +13,36 @@ module.exports = function (app) {
         res.render('document', { config: { prefix: config.http.prefix } });
     });
 
+    //无需增加额外路由
+    if (!config.http.prefix) return;
+
     /** 以下是针对配置单页路由后的文件重定向 **/
-    //获取封面
-    app.get(`${config.http.prefix}/_coverpage.md`, function (req, res, next) {
-        res.sendFile(config.dashboard.public || path.join(__dirname, '../../public/document/_coverpage.md'));
-    });
-    //获取内容
-    app.get(`${config.http.prefix}/README.md`, function (req, res, next) {
-        res.sendFile(config.dashboard.public || path.join(__dirname, '../../public/document/README.md'));
+    const forwardList = [
+        '/_coverpage.md',
+        '/README.md',
+        '/document/docsify.min.js',
+        '/document/vue.css',
+        '/document/fonts/css',
+        '/document/fonts/toadOcfmlt9b38dHJxOBGCP2LEk6lMzYsRqr3dHFImA.woff2',
+        '/document/fonts/ODelI1aHBYDBqgeIAH2zlJbPFduIYtoLzwST68uhz_Y.woff2',
+        '/document/fonts/toadOcfmlt9b38dHJxOBGMzFoXZ-Kj537nB_-9jJhlA.woff2',
+        '/document/fonts/hMqPNLsu_dywMa4C_DEpY4gp9Q8gbYrhqGlRav_IXfk.woff2'
+    ];
+
+    //设置路由
+    forwardList.forEach(fd => {
+        //进行初始化操作
+        const ext = path.extname(fd);
+        const extraPath = ext === '.md' && '/document/' || '';
+        const filePath = config.dashboard.public || path.join(__dirname, `../../public${extraPath}${fd}`);
+        //仅对 css 文件进行替换
+        const fileContent = String(fs.readFileSync(filePath));
+        const needReplace = ext === '.css' || !ext;
+        const newFileContent = needReplace && fileContent.replace(/\/document\/fonts\//g, `${config.http.prefix}/document/fonts/`);
+
+        //设置路由
+        app.get(`${config.http.prefix}${fd}`, function (req, res, next) {
+            needReplace ? res.end(newFileContent) : res.sendFile(config.dashboard.public || path.join(__dirname, `../../public${extraPath}${fd}`));
+        });
     });
 }
